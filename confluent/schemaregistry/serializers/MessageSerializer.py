3dfd1016212ec7788d2b57baa68eb182b9a537be
Zoltan Fedor <zoltan.fedor@thomsonreuters.com>
2015-10-26 20:25:39 +0000

    Undoing the HDFS package compatibility change
    As the HDFS package had to adjust, see https://github.com/mtth/hdfs/issues/32

--- confluent/schemaregistry/serializers/MessageSerializer.py
+++ confluent/schemaregistry/serializers/MessageSerializer.py
@@ -10,8 +10,7 @@ MAGIC_BYTE = 0
 
 HAS_FAST = False
 try:
-    #from fastavro.reader import read_data
-    import fastavro
+    from fastavro.reader import read_data
     HAS_FAST = True
 except:
     pass
@@ -144,13 +143,13 @@ class MessageSerializer(object):
             # try to use fast avro
             try:
                 schema_dict = schema.to_json()
-                obj = fastavro.reader(payload, schema_dict)
+                obj = read_data(payload, schema_dict)
                 # here means we passed so this is something fastavro can do
                 # seek back since it will be called again for the
                 # same payload - one time hit
 
                 payload.seek(curr_pos)
-                decoder_func = lambda p: fastavro.reader(p, schema_dict)
+                decoder_func = lambda p: read_data(p, schema_dict)
                 self.id_to_decoder_func[schema_id] = decoder_func
                 return self.id_to_decoder_func[schema_id]
             except:
